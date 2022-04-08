c99b158ca0a671a27ce9da0d3167b320d5f0d7c4
1aman1 <amangupta@outlook.com>
2022-04-08 12:42:13 +0530

    #include<array>

--- array.cpp
+++ array.cpp
@@ -1,31 +1,30 @@
 #include<array>
-#include<numeric>
 #include<iostream>
 using namespace std;
 
 int main() {
 	array<int, 6> arr;
-	
-	iota(arr.begin(), arr.end(), 0);
-	for (auto i : arr)
-		cout << i << " ";
-	cout << endl;
-	
-	//front and back provide ptr to the resp. position
-	cout << arr.front() + arr.back() << endl;
-		
-	//begin and end provide iterator to front and end
+
+  	( arr.empty() == true ) ? cout << "empty\n" : cout << "not empty\n";
+  
+	arr.fill(0);
+
+	for ( auto i : arr) cout << i << " "; cout << endl;
+
+  	// reference to front and back
+	arr.front() = arr.back() = 404;
+  
+  	// iterators
+	//begin and end provide iterator
 	for (auto it = arr.begin(); it != arr.end(); ++it)
-		cout << *it << " ";
-	cout << endl;
+		cout << *it << " "; cout << endl;
 	
-	array<int, 6> dummy;
+	array<int, arr.size()> dummy;
 	arr.swap(dummy);
 
-	for (auto i : dummy)
-		cout << i << " ";
-	cout << endl;
-	
-	system("pause");
-    return 0;
+	for ( auto i : dummy ) cout << i << " "; cout << endl;
+
+	for ( auto i : arr ) cout << i << " "; cout << endl;
+
+	return 0;
 }
