7219560ffbc3c7ee7d2ba16a2f7aa68ffffe21d6
1aman1 <amangupta@outlook.com>
2022-06-12 12:36:27 +0530

    +

--- bit manipulation/6 update Kth bit.cpp
+++ bit manipulation/6 update Kth bit.cpp
@@ -3,18 +3,18 @@
 
 int method1(int number, int Kth, int setto)
 {
-    int mask = !(1 << Kth);
-    return ((setto & mask) | (setto << Kth));
+    int mask = ~(1 << Kth);
+    return ((number & mask) | (setto << Kth));
 }
 
 int main()
 {
     int number = 3;
-    int Kth = 5;
+    int Kth = 4;
     int setto = 1;
 
     std::cout << number << ":" << std::bitset<8>(number) << " " << Kth << std::endl;
-    number = method1(number, Kth -1, setto);
+    number = method1(number, Kth - 1, setto);
     std::cout << number << ":" << std::bitset<8>(number) << std::endl;
 
     number = 15;
