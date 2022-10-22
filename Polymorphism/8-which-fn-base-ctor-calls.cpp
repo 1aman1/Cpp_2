40feb76c8343212596683f28aff9f3350baa57b1
1aman1 <amangupta@outlook.com>
2022-10-22 17:35:17 +0530

    +

---
+++ Polymorphism/8-which-fn-base-ctor-calls.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+// call funciton inside base ctor, that is both in base and der
+
+class base
+{
+public:
+	base()
+	{
+		run();
+	}
+
+	void
+	run()
+	{
+		std::cout << "run base\n";
+	}
+};
+
+class Derived : public base
+{
+public:
+	Derived() : base()
+	{
+		run();
+	}
+
+	void
+	run()
+	{
+		std::cout << "run derived\n";
+	}
+};
+
+int main()
+{
+	Derived o;
+
+	o.run();
+
+	return 0;
+}
