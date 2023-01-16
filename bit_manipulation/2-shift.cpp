141cbfc3c3a4e82fe479f9e079dbb65500534fee
1aman1 <amangupta@outlook.com>
2023-01-16 12:56:58 +0530

    revised

---
+++ bit_manipulation/2-shift.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+    int a = 25;
+
+    cout << a << " =a," << bitset<8>(a) << endl;
+    a = a >> 1;
+    cout << a << " =a," << bitset<8>(a) << endl;
+    
+    // cout << x;
+
+    return 0;
+}
--- bit_manipulation/6-update-Kth-bit.cpp
+++ bit_manipulation/6-update-Kth-bit.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <bitset>
 
-int method1(int number, int Kth)
+int Set_KthBit_As_Target(int number, int Kth, int target)
 {
-    return (number | (1 << Kth));
+    int mask = ~(1 << Kth);
+    return ((number & mask) | (target << Kth));
 }
 
 int main()
 {
-    int number = 10;
-    int Kth = 2;
-    
+    int number = 3;
+    int Kth = 4;
+    int target = 1;
+
     std::cout << number << ":" << std::bitset<8>(number) << " " << Kth << std::endl;
-    number = method1(number, Kth - 1);
+    number = Set_KthBit_As_Target(number, Kth - 1, target);
     std::cout << number << ":" << std::bitset<8>(number) << std::endl;
 
-    number = 16;
-    Kth = 2;
+    number = 15;
+    Kth = 3;
+    target = 0;
 
     std::cout << number << ":" << std::bitset<8>(number) << " " << Kth << std::endl;
-    number = method1(number, Kth - 1);
+    number = Set_KthBit_As_Target(number, Kth - 1, target);
     std::cout << number << ":" << std::bitset<8>(number) << std::endl;
 
     return 0;
